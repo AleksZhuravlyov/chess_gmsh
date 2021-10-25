ae26e661e0c24380258d109b7c9b1832e700cf5a
Aleksandr Zhuravlyov <a.s.zhuravljov@gmail.com>
2021-10-25 19:41:09 +0500

    Changed side quadrilaterals on triangles

--- gmsh_script.py
+++ gmsh_script.py
@@ -1,7 +1,6 @@
 import gmsh
 import json
 
-
 gmsh.initialize()
 gmsh.option.setNumber("Mesh.MshFileVersion", 2)
 gmsh.model.add("chess_gmsh")
@@ -11,101 +10,81 @@ lc = 1e-5
 with open('throats_polygons_edited.json') as file:
     model = json.load(file)
 
-inlet_throats = model['inlet_throats']
-outlet_throats = model['outlet_throats']
 hexahedrons = model['polygons']
 
-
 hex_n = len(hexahedrons)
-
 
 for polygon, curve_loops in hexahedrons.items():
     for curve_loop in curve_loops:
         for point in curve_loop:
             gmsh.model.occ.add_point(point[0], point[1], point[2])
-            
-for i in range(hex_n):
-    p0 = 1 + i * 8 + 0
-    p1 = 1 + i * 8 + 1
-    p2 = 1 + i * 8 + 2
-    p3 = 1 + i * 8 + 3
-    p4 = 1 + i * 8 + 4
-    p5 = 1 + i * 8 + 5
-    p6 = 1 + i * 8 + 6
-    p7 = 1 + i * 8 + 7    
-    
-    gmsh.model.occ.add_line(p0, p1) # 0
-    gmsh.model.occ.add_line(p1, p3) # 1
-    gmsh.model.occ.add_line(p3, p2) # 2
-    gmsh.model.occ.add_line(p2, p0) # 3
-                    
-    gmsh.model.occ.add_line(p4, p5) # 4
-    gmsh.model.occ.add_line(p5, p7) # 5
-    gmsh.model.occ.add_line(p7, p6) # 6
-    gmsh.model.occ.add_line(p6, p4) # 7
-    
-    gmsh.model.occ.add_line(p0, p6) # 8
-    gmsh.model.occ.add_line(p1, p7) # 9
-    gmsh.model.occ.add_line(p3, p5) # 10
-    gmsh.model.occ.add_line(p2, p4) # 11
-    
-for i in range(hex_n):
-    l0 = 1 + i * 12 + 0
-    l1 = 1 + i * 12 + 1
-    l2 = 1 + i * 12 + 2
-    l3 = 1 + i * 12 + 3
-    l4 = 1 + i * 12 + 4
-    l5 = 1 + i * 12 + 5
-    l6 = 1 + i * 12 + 6
-    l7 = 1 + i * 12 + 7
-    l8 = 1 + i * 12 + 8
-    l9 = 1 + i * 12 + 9
-    l10 = 1 + i * 12 + 10
-    l11 = 1 + i * 12 + 11    
-    
-    gmsh.model.occ.add_curve_loop([l0, l1, l2, l3])
-    gmsh.model.occ.add_curve_loop([l4, l5, l6, l7])
-    gmsh.model.occ.add_curve_loop([-l2, l10, -l4, -l11])
-    gmsh.model.occ.add_curve_loop([-l0, l8, -l6, -l9])
-    gmsh.model.occ.add_curve_loop([-l1, l9, -l5, -l10])
-    gmsh.model.occ.add_curve_loop([l3, l8, l7, -l11])
- 
- 
-for i in range(hex_n):
-    ls0 = 1 + i * 6 + 0
-    ls1 = 1 + i * 6 + 1
-    ls2 = 1 + i * 6 + 2
-    ls3 = 1 + i * 6 + 3
-    ls4 = 1 + i * 6 + 4
-    ls5 = 1 + i * 6 + 5
-      
-    gmsh.model.occ.add_plane_surface([ls0])
-    gmsh.model.occ.add_plane_surface([ls1])
-    gmsh.model.occ.add_plane_surface([ls2])
-    gmsh.model.occ.add_plane_surface([ls3])
-    gmsh.model.occ.add_plane_surface([ls4])
-    gmsh.model.occ.add_plane_surface([ls5])
-    
-    
-for i in range(hex_n):
-    s0 = 1 + i * 6 + 0
-    s1 = 1 + i * 6 + 1
-    s2 = 1 + i * 6 + 2
-    s3 = 1 + i * 6 + 3
-    s4 = 1 + i * 6 + 4
-    s5 = 1 + i * 6 + 5
-      
-    gmsh.model.occ.add_volume([gmsh.model.occ.add_surface_loop([s0, s1, s2, s3, s4, s5])])
 
+add_line = gmsh.model.occ.add_line
+for i in range(hex_n):
+    start = 1 + i * 8 + 0
+    stop = 1 + i * 8 + 7 + 1
+    ps = range(start, stop)
 
-gmsh.model.occ.fuse([(3, 1)], [(3, i) for i in range(2, hex_n + 1)])
+    add_line(ps[0], ps[1])  # 0
+    add_line(ps[1], ps[3])  # 1
+    add_line(ps[3], ps[2])  # 2
+    add_line(ps[2], ps[0])  # 3
+
+    add_line(ps[4], ps[5])  # 4
+    add_line(ps[5], ps[7])  # 5
+    add_line(ps[7], ps[6])  # 6
+    add_line(ps[6], ps[4])  # 7
+
+    add_line(ps[0], ps[6])  # 8
+    add_line(ps[1], ps[7])  # 9
+    add_line(ps[3], ps[5])  # 10
+    add_line(ps[2], ps[4])  # 11
+
+    add_line(ps[0], ps[7])  # 12
+    add_line(ps[3], ps[4])  # 13
+    add_line(ps[1], ps[5])  # 14
+    add_line(ps[6], ps[2])  # 15
+
+add_curve_loop = gmsh.model.occ.add_curve_loop
+add_plane_surface = gmsh.model.occ.add_plane_surface
+for i in range(hex_n):
+    start = 1 + i * 16 + 0
+    stop = 1 + i * 16 + 15 + 1
+    ls = range(start, stop)
+
+    add_plane_surface([add_curve_loop([ls[0], ls[1], ls[2], ls[3]])])  # 0
+
+    add_plane_surface([add_curve_loop([ls[4], ls[5], ls[6], ls[7]])])  # 1
+
+    add_plane_surface([add_curve_loop([ls[12], ls[6], -ls[8]])])  # 2
+    add_plane_surface([add_curve_loop([ls[0], ls[9], -ls[12]])])  # 3
+
+    add_plane_surface([add_curve_loop([ls[14], ls[5], -ls[9]])])  # 4
+    add_plane_surface([add_curve_loop([ls[1], ls[10], -ls[14]])])  # 5
+
+    add_plane_surface([add_curve_loop([ls[13], ls[4], -ls[10]])])  # 6
+    add_plane_surface([add_curve_loop([ls[2], ls[11], -ls[13]])])  # 7
+
+    add_plane_surface([add_curve_loop([ls[15], ls[11], -ls[7]])])  # 8
+    add_plane_surface([add_curve_loop([ls[3], ls[8], -ls[15]])])  # 9
+
+add_surface_loop = gmsh.model.occ.add_surface_loop
+add_volume = gmsh.model.occ.add_volume
+for i in range(hex_n):
+    start = 1 + i * 10 + 0
+    stop = 1 + i * 10 + 9 + 1
+    add_volume([add_surface_loop(range(start, stop))])
+
+objectDimTags = [(3, 1)]
+for i in range(2, hex_n + 1):
+    objectDimTags = gmsh.model.occ.fuse(objectDimTags, [(3, i)])[0]
 
 gmsh.model.occ.synchronize()
 
 s_left = []
 for s in gmsh.model.getEntitiesInBoundingBox(-1e-2, -1e-2, -1e-2, 1e-5, 1e-3, 1e-4, 2):
     s_left.append(s[1])
-    
+
 s_right = []
 for s in gmsh.model.getEntitiesInBoundingBox(8e-4, -1e-2, -1e-2, 12e-4, 1e-3, 1e-4, 2):
     s_right.append(s[1])
@@ -115,8 +94,9 @@ gmsh.model.setPhysicalName(2, gmsh.model.addPhysicalGroup(2, s_right), "right")
 
 gmsh.model.setPhysicalName(3, gmsh.model.addPhysicalGroup(3, [e[1] for e in gmsh.model.getEntities(3)]), "volume")
 
-
 gmsh.model.mesh.setSize(gmsh.model.getEntities(0), lc)
+
+# gmsh.option.setNumber("Mesh.SaveAll", 1)
 
 gmsh.model.mesh.generate(3)
 
